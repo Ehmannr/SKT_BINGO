ecd0d7d27af568143c47e0ef32b2f529a1d13127
Ehmannr <ehmannr@etown.edu>
2021-09-06 10:53:15 -0400

    Update insides.js

---
+++ insides.js
@@ -0,0 +1,22 @@
+const master = [
+    'talks about drugs (in game)', 'Someone has sex', 'Bill Ehmann cameo','Goof goes into normal voice for seroius talk','NEED SOMETHING',
+    'NPC Impersonation', 'Amber Smokes pot','Earl rolls more than 8 dice in a turn','Making fun of inteligence of DM','Big iron is mentioned',
+    'Mot starts rambling about infomation gathered','Vel uses shocking grasp','home is played','Something gets puts into the Quotes channel','goof breaks down something to wake mot up',
+    'Kyle says "something my char would know"','Someone makes a deal with garfield','in game racism','Fin goes fishing','Earl mentioned squeekers',
+    'joke about forest fire','DM has more than 4 voices','Fin slaps something to death','Someone is late','D20 dice montage'
+];
+//Note please spell check this later since Ryan Wrote this
+
+var indexes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+
+function getinside (){
+    //pick a number between 0 ->24
+    //put number in array 
+    //check if number is in array
+    //if not put in taken, if taken pick new number
+    //return master[number]
+    //go to loop
+
+
+    var taken = new array ()
+}
