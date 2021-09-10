eb7ea0a3f3ceb5c1c5b92b110d2af358e8a0168e
Ehmannr <ehmannr@etown.edu>
2021-09-10 00:24:25 -0400

    Fixed spelling

--- insideSkt.js
+++ insideSkt.js
@@ -1,13 +1,13 @@
 const master = [
-    'talks about drugs (in game)', 'Someone has sex', 'Bill Ehmann cameo', 'Goof goes into normal voice for seroius talk', 'NEED SOMETHING',
+    'Talks about drugs ', 'Someone has sex', 'Bill Ehmann cameo', 'Goof goes into normal voice for seroius talk', 'NEED SOMETHING',
 
-    'NPC Impersonation', 'Amber Smokes pot', 'Earl rolls more than 8 dice in a turn', 'Making fun of inteligence of DM', 'Big iron is mentioned',
+    'Need something', 'Vel Smokes pot', 'Earl rolls more than 8 dice in a turn', 'Make fun of DM', 'Big iron is mentioned',
 
-    'Mot starts rambling about infomation gathered', 'Vel uses shocking grasp', 'home is played', 'Something gets puts into the Quotes channel', 'goof breaks down something to wake mot up',
+    'Mot starts rambling', 'Vel uses shocking grasp', 'home is played', 'Something gets puts into the Quotes channel', 'goof breaks down something to wake mot up',
 
-    'Kyle says "something my char would know"', 'Someone makes a deal with garfield', 'in game racism', 'Fin goes fishing', 'Earl mentioned squeekers',
+    '"Something my char would know"', 'Someone makes a deal with garfield', 'In game racism', 'Fin goes fishing', 'NEED SOMETHING',
 
-    'joke about forest fire', 'DM has more than 4 voices', 'Fin slaps something to death', 'Someone is late', 'D20 dice montage'
+    'jJke about forest fire', 'DM has more than 4 voices', 'Fin slaps something to death', 'Someone is late', 'D20 dice montage'
 ];
 //Note please spell check this later since Ryan Wrote this
 
