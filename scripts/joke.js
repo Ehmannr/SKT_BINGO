615b679a9b2a137c278715b11954f913ebf504fc
Ehmannr <ehmannr@etown.edu>
2021-09-17 11:45:50 -0400

    added dividers

--- scripts/joke.js
+++ scripts/joke.js
@@ -1,10 +1,11 @@
 function badBackground() {
 
-    if (document.querySelector('.orange-background')) {
+    if (document.querySelector('#bingo')) {
         var num = Math.floor(Math.random() * 20) + 1
-        if (num == 1) {
+        if (num== 1) {
             document.body.classList.toggle("new-background")
             document.body.classList.toggle("bad-background")
+            document.querySelector('#main_wrapper').classList.toggle("bad-background")
             document.querySelector('#bingo').innerHTML = "I hope you enjoy this Jimmy"
         }
 
