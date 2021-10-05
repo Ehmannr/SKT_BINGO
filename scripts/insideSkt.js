bde37fd1145a6ab66bf7c665e6aebbafd4c6d9ef
Ryan Ehmann <RyanKEhmann@gmail.com>
2021-10-04 23:40:13 -0400

    fixed infinite songs

--- scripts/insideSkt.js
+++ scripts/insideSkt.js
@@ -20,6 +20,7 @@ const master = [
 var usedindex = []
 let allBoxes = [...document.querySelectorAll('.box')]
 var temp = 0;
+var bingo = false
 
 function fillIndex() {
     while (temp < 25) {
@@ -85,83 +86,84 @@ function checkBoard() {
     //left to right
     //  0   1   2    3   4
     if (allBoxes[0].classList.contains('used') && allBoxes[1].classList.contains('used') && allBoxes[2].classList.contains('used') && allBoxes[3].classList.contains('used') && allBoxes[4].classList.contains('used')) {
-        BINGOBONGOTIME()
+        BINGOBONGOTIME(bingo)
     }
     //  5   6   7   8   9
     if (allBoxes[5].classList.contains('used') && allBoxes[6].classList.contains('used') && allBoxes[7].classList.contains('used') && allBoxes[8].classList.contains('used') && allBoxes[9].classList.contains('used')) {
-        BINGOBONGOTIME()
+        BINGOBONGOTIME(bingo)
     }
     //  10  11  12  13  14
     if (allBoxes[10].classList.contains('used') && allBoxes[11].classList.contains('used') && allBoxes[12].classList.contains('used') && allBoxes[13].classList.contains('used') && allBoxes[14].classList.contains('used')) {
-        BINGOBONGOTIME()
+        BINGOBONGOTIME(bingo)
     }
     //  15  16  17  18  19
     if (allBoxes[15].classList.contains('used') && allBoxes[16].classList.contains('used') && allBoxes[17].classList.contains('used') && allBoxes[18].classList.contains('used') && allBoxes[19].classList.contains('used')) {
-        BINGOBONGOTIME()
+        BINGOBONGOTIME(bingo)
     }
     //  20  21  22  23  24
     if (allBoxes[20].classList.contains('used') && allBoxes[21].classList.contains('used') && allBoxes[22].classList.contains('used') && allBoxes[23].classList.contains('used') && allBoxes[24].classList.contains('used')) {
-        BINGOBONGOTIME()
+        BINGOBONGOTIME(bingo)
     }
     //Diag
     //  0   6   12  18  24
     if (allBoxes[0].classList.contains('used') && allBoxes[6].classList.contains('used') && allBoxes[12].classList.contains('used') && allBoxes[18].classList.contains('used') && allBoxes[24].classList.contains('used')) {
-        BINGOBONGOTIME()
+        BINGOBONGOTIME(bingo)
     }
     //  4   8   12  16  20
     if (allBoxes[4].classList.contains('used') && allBoxes[8].classList.contains('used') && allBoxes[12].classList.contains('used') && allBoxes[16].classList.contains('used') && allBoxes[20].classList.contains('used')) {
-        BINGOBONGOTIME()
+        BINGOBONGOTIME(bingo)
     }
     //  B
     if (allBoxes[0].classList.contains('used') && allBoxes[5].classList.contains('used') && allBoxes[10].classList.contains('used') && allBoxes[15].classList.contains('used') && allBoxes[20].classList.contains('used')) {
-        BINGOBONGOTIME()
+        BINGOBONGOTIME(bingo)
     }
     //  I
     if (allBoxes[1].classList.contains('used') && allBoxes[6].classList.contains('used') && allBoxes[11].classList.contains('used') && allBoxes[16].classList.contains('used') && allBoxes[21].classList.contains('used')) {
-        BINGOBONGOTIME()
+        BINGOBONGOTIME(bingo)
     }
     //  n
     if (allBoxes[2].classList.contains('used') && allBoxes[7].classList.contains('used') && allBoxes[12].classList.contains('used') && allBoxes[17].classList.contains('used') && allBoxes[22].classList.contains('used')) {
-        BINGOBONGOTIME()
+        BINGOBONGOTIME(bingo)
     }
     //  g
     if (allBoxes[3].classList.contains('used') && allBoxes[8].classList.contains('used') && allBoxes[13].classList.contains('used') && allBoxes[18].classList.contains('used') && allBoxes[23].classList.contains('used')) {
-        BINGOBONGOTIME()
+        BINGOBONGOTIME(bingo)
     }
     //  O
     if (allBoxes[4].classList.contains('used') && allBoxes[9].classList.contains('used') && allBoxes[14].classList.contains('used') && allBoxes[19].classList.contains('used') && allBoxes[24].classList.contains('used')) {
-        BINGOBONGOTIME()
+        BINGOBONGOTIME(bingo)
     }
 }
 
  function BINGOBONGOTIME() {
-
+     if(bingo== false){
     clearUsed()
-    
    audio = pickASong()
    play()
-   
-    
-   
-   window.setInterval(DiscoTime, 1000)
+
+   window.setInterval(DiscoTime, 500)
     confetti.start()
     document.querySelector('#card__wrapper').classList.toggle('lightning-background')
     document.querySelector('#card__wrapper').classList.toggle('DISCO')
     changeBorder('black')
+    bingo = true
+}
 
 }
 
  function DiscoTime() {
 
-   
-
+    
+    
     index = Math.floor(Math.random() * 24)
     colorInt = Math.floor(Math.random() * 4)+1
-    //console.log(colorInt)
+    
 
+    
     switch (colorInt) {
         case 1:
             allBoxes[index].classList.toggle('red-bg')
+            
             break;
         case 2:
             allBoxes[index].classList.toggle('greenDisco-bg')
@@ -173,33 +175,28 @@ function checkBoard() {
             allBoxes[index].classList.toggle('purp-bg')
             break;
     }
+
+   
 }
 
  
 function pickASong(){
 
-    songChoise = Math.floor(Math.random() * 3)+1
-    console.log(songChoise)
+    songChoise = Math.floor(Math.random() * 2)+1
+   // console.log(songChoise)
 
 switch (songChoise) {
         case 1: //CHYHN
-            var audio = document.getElementById("myAudio2");
-            
+            var audio = document.getElementById("myAudio2"); 
         
-        return audio;
+            return audio;
             
         case 2:
             var audio = document.getElementById("myAudio");
            
-            
-            
             return audio;
           
-        case 3:
-            var audio = document.getElementById("myAudio3");
-
-            return audio;
-            
+      
        
     }
 }
@@ -212,7 +209,7 @@ function clearUsed() {
     while (temp < 25) {
         if (allBoxes[temp].classList.contains('used')) {
             allBoxes[temp].classList.toggle('used');
-            console.log(temp)
+            //console.log(temp)
         }
         temp++
     }
